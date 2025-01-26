8f8cc468ee16d397135eedd96197aaaec94247cb
Polclard <alenjang007@gmail.com>
2025-01-27 00:10:46 +0100

    Изложби и автори може да бидат додадени само од супер-корисници

--- artexhebitionapp/admin.py
+++ artexhebitionapp/admin.py
@@ -5,3 +5,31 @@ from artexhebitionapp.models import Exhibition, Artist, Art
 
 # Register your models here.
 
+class ArtAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        super().save_model(request, obj, form, change)
+
+class ArtistAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            return True
+
+class ExhibitionAdmin(admin.ModelAdmin):
+
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            return True
+
+
+admin.site.register(Art, ArtAdmin)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Exhibition, ExhibitionAdmin)
