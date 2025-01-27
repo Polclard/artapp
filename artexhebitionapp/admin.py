9838816cc17ba3921b9b989100749308cd6f99d8
Polclard <alenjang007@gmail.com>
2025-01-27 15:22:20 +0100

    Уметнички дела можат да бидат додадени само од уметници и уметникот по автоматизам се додава како автор на делото

--- artexhebitionapp/admin.py
+++ artexhebitionapp/admin.py
@@ -6,28 +6,44 @@ from artexhebitionapp.models import Exhibition, Artist, Art
 # Register your models here.
 
 class ArtAdmin(admin.ModelAdmin):
+    readonly_fields = ('artist',)
+    def has_module_permission(self, request):
+        return hasattr(request.user, 'arts')
+
+    def has_add_permission(self, request):
+        return hasattr(request.user, 'arts')
+
+    def has_change_permission(self, request, obj=None):
+        return hasattr(request.user, 'arts')
+
+    def has_delete_permission(self, request, obj=None):
+        return hasattr(request.user, 'arts')
+
+    def has_view_permission(self, request, obj=None):
+        return hasattr(request.user, 'arts')
 
     def save_model(self, request, obj, form, change):
-        obj.user = request.user
+        found_artist = Artist.objects.filter(user=request.user).first()
+        obj.artist = found_artist
         super().save_model(request, obj, form, change)
 
-class ArtistAdmin(admin.ModelAdmin):
 
+class ArtistAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         if request.user.is_superuser:
             return True
     def save_model(self, request, obj, form, change):
         if request.user.is_superuser:
-            return True
+            super().save_model(request, obj, form, change)
+
 
 class ExhibitionAdmin(admin.ModelAdmin):
-
     def has_add_permission(self, request):
         if request.user.is_superuser:
             return True
     def save_model(self, request, obj, form, change):
         if request.user.is_superuser:
-            return True
+            super().save_model(request, obj, form, change)
 
 
 admin.site.register(Art, ArtAdmin)
